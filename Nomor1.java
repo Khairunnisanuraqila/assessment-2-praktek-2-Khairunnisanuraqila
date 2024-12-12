96bf638aeff4918013cfd85201905a12f19641c2
Khairunnisanuraqila <khairunnisanuraqila121@gmail.com>
2024-12-12 10:53:11 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,15 +1,27 @@
 import java.util.Scanner;
 
-public class Nomor1 {
-
-    public static void main(String[] args){
+public class soal1 {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
 
-        /* Pekerjaan anda berakhir sini */
-
-        System.out.println(Nomor1.class.getDeclaredMethods().length); 
+        System.out.print("Masukan nilai N : ");
+        int n = input.nextInt();
+        
+        System.out.println("Deret FizzBuzz dari 0 hingga " + n + ":");
+        tampilkanDeretFizzBuzz(n);
     }
-    
+
+    public static void tampilkanDeretFizzBuzz(int n) {
+        for (int i = 0; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FizzBuzz ");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
